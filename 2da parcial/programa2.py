6d007f06bfe415d129d3d7ec2f91346a421b370c
Edson Paul <73849076+EdsonMorfin@users.noreply.github.com>
2022-05-17 13:37:38 -0500

    Add files via upload

---
+++ 2da parcial/programa2.py
@@ -0,0 +1,11 @@
+
+
+import nltk
+from nltk.corpus import treebank
+
+sentence = "./Documentos/documento2.txt"
+tokens = nltk.word_tokenize(sentence)
+print(tokens)
+
+t = treebank.parsed_sents('wsj_0001.mrg')[0]
+t.draw()
